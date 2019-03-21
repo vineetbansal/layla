45f55acaa91cef857402852e1b1b0a483cd3c32d
Vineet Bansal <vineetbansal@gmail.com>
2019-03-21 17:05:00 -0400

    moved source inside a 'src' folder.

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
-from setuptools import setup, find_packages
-import layla
+from setuptools import setup, find_namespace_packages
 
 
 setup(
     name="layla",
-    version=layla.__version__,
-    packages=find_packages(),
+    version="0.1.4",
 
-    # For including non-source files, specify the wildcard path here
-    # The key is the package name though, so in this case the layla/data/ folder needs an __init__.py
-    # Simply including the file says nothing about how we're going to load it though
-    # (which is where importlib_resources>=1.0.2 comes in)
-    package_data={
-        'layla.data': ['*.txt']
-    },
+    package_dir={'': 'src'},
+
+    # The advantage of using `find_namespace_packages` instead of the commonly-used `find_packages` is that
+    # the latter will discover a top-level `tests` package, if any, and include it directly in the environment's
+    # `site-packages` folder, at the top level, detached from our main package, which is never what we want.
+    # This way we are forced to be explicit about whether to include `tests` in our distribution or not.
+    packages=find_namespace_packages(where='src'),
 
     # Should setuptools consult the MANIFEST.in file to see what else needs to be included?
     # Again, this has nothing to do with how we would go about loading it in our code
     # (which is tricky if it cannot be reached through the python package loading mechanism - since the whole
     # source folder might be zipped up, we cannot rely on os.path etc. mechanisms)
+    # This is where importlib_resources>=1.0.2 comes in
     include_package_data=True,
 
     # What does the project *minimally* need to run correctly?
@@ -30,5 +29,10 @@ setup(
 
     install_requires=[
         'importlib_resources>=1.0.2'
-    ]
+    ],
+
+    # For this to work without a lot of fuss, make sure that 'tests' is a package by including an __init__.py
+    test_suite='tests',
 )
+
+
